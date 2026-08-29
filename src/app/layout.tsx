10fc282339d941a7e8fc7acbdc8ee18a7fac3fde
Ethan <ethan@localhost>
2026-08-29 14:20:17 -0400

    injecting analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 import { Providers } from "@/app/providers";
 
 const geistSans = Geist({
@@ -29,6 +29,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     >
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
